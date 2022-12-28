f285ab5b5d38d7fc7102e846b2935df5d04ef170
Tito Rhenaldi P <tito.rhenaldi@psn.co.id>
2022-12-28 10:51:26 +0700

    Update Routing
    + support fragmetn scrolls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./pages/pages.module').then((m) => m.PagesModule),
+      },
+    ],
+  },
+];
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled', //On change of router url the position of the screen will set to the top.
+  anchorScrolling: 'enabled', //When set to ‘enabled’, scrolls to the anchor element when the URL has a fragment. Anchor scrolling is disabled by default.
+  scrollOffset: [0, 64], //Configures the scroll offset the router will use when scrolling to an element.
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, routerOptions)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
