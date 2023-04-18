8365cd51d3c0ea97cedb298798adf8a1ed74bc84
Tito Rhenaldi P <tito.rhenaldi@psn.co.id>
2023-04-18 11:31:38 +0700

    Add Information on Timeline

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -11,25 +11,29 @@ export class ExperienceComponent implements OnInit {
     {
       status: 'Graduated: Brawijaya University',
       date: '08/2017 - 07/2021',
-      icon: undefined,
-      number: 1,
-      color: '#9C27B0',
-      image: 'game-controller.jpg',
+      icon: PrimeIcons.CHECK_CIRCLE,
+      number: undefined,
+      color: '#14B8A6',
+      description_en: `Graduated with a bachelor degree in Information System and a 3.45 GPA. I've also published one publication about Data Warehouse development. <br> <br>
+      While studying here, I've learned alot about the basic of programming, IT infrastructure management, and UI/UX. Beside that, I've also practice and honed my soft skill (such as my team work ability and communication skills).`,
     },
     {
       status: 'Working: PT. Pasifik Satelit Nusantara',
       date: '10/2021 - Ongoing',
-      icon: PrimeIcons.CHECK_CIRCLE,
+      icon: PrimeIcons.BOOKMARK_FILL,
       number: undefined,
-      color: '#673AB7',
+      color: '#007ad9',
+      description_en: `PT. Pasifik Satelit Nusantara or PSN is a telecommunication company that handles in manufacturing satellite and providing affordable internet in rural areas. <br><br>
+      I'm currently working as a frontend web developer here. Most of my days consist of creating dashboards to monitor those satellites and other variables that may effect them (e.g weather). I also help in mantaining the company Business Support System (BSS) for internal and external purposes. <br><br>
+      I mostly use Angular as my frontend framework, but I've also helped some project using django and Code Igniter as the framework.`,
     },
-    {
-      status: 'Shipped',
-      date: '15/10/2020 16:15',
-      icon: PrimeIcons.ENVELOPE,
-      number: undefined,
-      color: '#FF9800',
-    },
+    // {
+    //   status: 'The Future',
+    //   icon: PrimeIcons.QUESTION_CIRCLE,
+    //   number: undefined,
+    //   color: '#9C27B0',
+    //   description_en: `I'm currently not looking for work, but `,
+    // },
   ];
 
   constructor() {}
