53b1d7e6db79e8c0c556e319a41328cff311cb35
Tito Rhenaldi P <tito.rhenaldi@psn.co.id>
2023-07-05 09:50:38 +0700

    Add Fragment Detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tito Rhenaldi';
+
+  public sections = ['home', 'about', 'experience', 'contact'];
+
+  constructor(private router: Router) {}
+
+  isIntersecting(status: boolean, element: string) {
+    console.log('Element ' + element + ' is intersecting ' + status);
+
+    if (!status) return;
+
+    this.router.navigate([], { fragment: element });
+  }
 }
