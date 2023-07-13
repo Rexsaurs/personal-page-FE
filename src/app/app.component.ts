c356ec9cc52cc1e0703452bde5740448ef51e1dd
Tito Rhenaldi P <tito.rhenaldi@psn.co.id>
2023-07-13 11:06:21 +0700

    Remove Intersection observer for sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,5 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'Tito Rhenaldi';
 
-  public sections = ['home', 'about', 'experience', 'contact'];
-
   constructor(private router: Router) {}
-
-  isIntersecting(status: boolean, element: string) {
-    console.log('Element ' + element + ' is intersecting ' + status);
-
-    if (!status) return;
-
-    this.router.navigate([], { fragment: element });
-  }
 }
